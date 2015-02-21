0644088f4522a74fe50f5e6b59d448fbf3177314
Charles Park <charlespk3@gmail.com>
2015-02-20 22:19:54 -0600

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	philomaths/grocerylist/views.py

--- philomaths/grocerylist/models.py
+++ philomaths/grocerylist/models.py
@@ -1,4 +1,20 @@
 from django.db import models
+import uuid
+
+class UUIDField(models.CharField) :
+    
+    def __init__(self, *args, **kwargs):
+        kwargs['max_length'] = kwargs.get('max_length', 64 )
+        kwargs['blank'] = True
+        models.CharField.__init__(self, *args, **kwargs)
+    
+    def pre_save(self, model_instance, add):
+        if add :
+            value = str(uuid.uuid4())
+            setattr(model_instance, self.attname, value)
+            return value
+        else:
+            return super(models.CharField, self).pre_save(model_instance, add)
 
 class Store(models.Model):
     latitude = models.FloatField()
@@ -25,3 +41,4 @@ class Product(models.Model):
 class List(models.Model):
     products = models.ManyToManyField(Product)
     name = models.TextField()
+    uuid = UUIDField(primary_key=True, editable=False)
