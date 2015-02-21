45e4891fb8fd116592bc02f1ae8a174c7ce9dfbb
l33t <you@example.com>
2015-02-20 19:46:57 -0600

    Add create list page

---
+++ philomaths/grocerylist/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import patterns, url
+
+from grocerylist import views
+
+urlpatterns = patterns('',
+    url(r'^$', views.index, name='index'),
+    url(r'^displayresult$', views.displayresult, name='displayresult'),
+    url(r'^createlist$', views.createlist, name='createlist'),
+    )
--- philomaths/grocerylist/views.py
+++ philomaths/grocerylist/views.py
@@ -1,6 +1,31 @@
 from django.template import RequestContext, loader
 from django.http import HttpResponse
 from django.shortcuts import render
+from django import forms
+from django.forms import ModelForm
+
+from grocerylist.models import List, Product
+
+class ListForm(forms.Form):
+    products = forms.ModelMultipleChoiceField(queryset=Product.objects.all())
+
+class ProductForm(ModelForm):
+    class Meta:
+        model = Product
+
+
+def createlist(request):
+    if request.method == 'POST':
+        pass
+    
+    item_list = ['boob', 'penis']
+    template = loader.get_template('createlist.html')
+    form = ListForm()
+    context = RequestContext(request, {
+        'item_list' : item_list,
+        'form' : form.as_ul(),
+    })
+    return HttpResponse(template.render(context))
 
 def displayresult(request):
     template = loader.get_template('displayresult.html')
